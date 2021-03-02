7d874d140d90f8cfd93a959463b031a023f0c59e
Ryabaa <73956155+Ryabaa@users.noreply.github.com>
2021-03-02 19:55:51 +0200

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
 exports.min = function min (array) {
+  if (array && array.length) {
+    return Math.min.apply(null, array);
+  }
+
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length) {
+    return Math.max.apply(null, array);
+  }
+
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length) {
+    const reducer = (a, v) => a + v;
+    return array.reduce(reducer) / array.length;
+  }
+
   return 0;
 }
